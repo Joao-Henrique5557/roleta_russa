04e8ba527ee0791b5d38e0c2538da5d185c67d91
joao-henrique <7joaohenrique5557@gmail.com>
2026-04-25 17:58:01 -0300

    Atualizando revolve

--- src/roleta_russa/Revolve.java
+++ src/roleta_russa/Revolve.java
@@ -1,19 +1,40 @@
 package roleta_russa;
 
-import java.util.List;
+import java.util.Arrays;
 
 public class Revolve {
 	private int quantBalas;
 	private boolean[] balas;
 	private int quantBalasVerdadeiras;
 	
-	public Revolve(String dificuldade) {
+	public void atirar() {
+		
+	}
+	
+	private void sortearBalaVerdadeira() {
+		if (this.quantBalas == 0) {
+			System.out.println("Erro");
+		}
+		
+		if (this.balas == null) {
+			System.out.println("Erro");
+		}
+		
+		int posicaoVerdadeira = (int) (Math.random() * (this.quantBalas)) ; // numero aleatorio entre 0 e 6
+		if (this.balas[posicaoVerdadeira] == true) {
+			sortearBalaVerdadeira();
+		} else {
+			this.balas[posicaoVerdadeira] = true;
+		}
+	}
+	
+	public void rearmazenarBalas(String dificuldade) {
 		this.quantBalas = (int) (Math.random()*5) + 2; // numero entre 2 e 6
 		
 		switch(dificuldade){
 			case "facil":
 				this.quantBalasVerdadeiras = 1;
-				if (this.quantBalas >= 3) {
+				if (this.quantBalas >= 3) { // balas podem ser 2 ou 3 contra 1 bala V
 					this.quantBalas = 3;
 				}
 				break;
@@ -25,15 +46,48 @@ public class Revolve {
 				break;
 			case "dificil":
 				this.quantBalasVerdadeiras = 3;
-				this.quantBalas += 2;
+				if (this.quantBalas <= 3 || this.quantBalas < 6) {
+					this.quantBalas += 1;
+				}
 				break;
 		}
+		
+		this.balas = new boolean[this.quantBalas]; // exemplo 6
+		
+		for(int i=0; i<this.quantBalasVerdadeiras; i++) { // exemplo: balas verdadeiras: 2
+			sortearBalaVerdadeira();
+		}
+	}
+	
+	public void mostrarInfoDoRevolve() {
+		if(this.quantBalas == 0) {
+			System.out.println("Erro");
+			return;
+		}
+		if(this.quantBalasVerdadeiras == 0) {
+			System.out.println("Erro");
+			return;
+		}
+		
+		System.out.println("numero de balas: " + this.quantBalas);
+		System.out.println("numero de balas verdadeiras: " + this.quantBalasVerdadeiras);
+	}
+	
+	public void mostrarInfoDoRevolve(String lista) {
+		if(this.quantBalas == 0) {
+			System.out.println("Erro");
+			return;
+		}
+		if(this.quantBalasVerdadeiras == 0) {
+			System.out.println("Erro");
+			return;
+		}
+		
 		System.out.println("numero de balas: " + this.quantBalas);
 		System.out.println("numero de balas verdadeiras: " + this.quantBalasVerdadeiras);
 		
-	}
-
-	public void atirar() {
-		
+		if (lista == "lista") {
+			System.out.println("balas: " + Arrays.toString(balas));
+		}
 	}
 }
--- src/roleta_russa/Rodada.java
+++ src/roleta_russa/Rodada.java
@@ -3,7 +3,10 @@ package roleta_russa;
 public class Rodada {
 	
 	public Rodada(String dificuldade) {
-		Revolve rvv = new Revolve(dificuldade);
+		Revolve rvv = new Revolve();
+		rvv.rearmazenarBalas(dificuldade);
+		rvv.mostrarInfoDoRevolve("lista");
+		
 		System.out.println("O primeiro a jogar é você, você atira em si mesmo ou no oponente?");
 	}
 }
